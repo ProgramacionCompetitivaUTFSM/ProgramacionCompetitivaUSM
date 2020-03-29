cf5ebb6a6feecc1c2c7a1a415aa2eda404c6a3e8
Anastasiia Fedorova <anastasiia.fedorova.s@gmail.com>
2020-03-28 23:51:22 -0300

    1002 (cpp)

---
+++ soluciones/URI/problemas/principiante/1002/1002_argentumx.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define PI 3.14159
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.setf(ios::fixed);
+  cout.precision(4);
+
+  //Code
+  double R;
+  cin >> R;
+  cout << "A=" << PI * R * R << "\n";
+  return 0;
+}

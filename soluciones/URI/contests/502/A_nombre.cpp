0a540cdd6224111e0eef052195a1a32586810555
Anastasiia Fedorova <anastasiia.fedorova.s@gmail.com>
2020-03-28 21:47:36 -0300

    dummy de commit

---
+++ soluciones/URI/contests/502/A_nombre.cpp
@@ -0,0 +1,9 @@
+// Aqui va su codigo
+// Recuerden comentar lo que puede ser no tan obvio
+// Para agregar su solucion a la tabla de README.md, seguir los ejemplos dados.
+// Los campos de complejidad no son obligatorios, en caso de no querer rellenarlos, dejenlos con un -
+// El campo de comentario es un link de tipo [link](link_relativo_a_su_seccion_de_markdown_que_esta_en_carpeta_de_contest_especifo)
+// Donde uds pueden dejar su comentario/explicacion/cualquier otra cosa sobre el problema relacionado
+// Bajo la seccion 
+## Nombre_Problema
+### su_nombre_en_git <- esto es lo que deben linkear en la tabla.
